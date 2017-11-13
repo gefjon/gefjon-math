e3fc713c548330dadab6d0c1100870d2125b21cd
Arthur Goldman <arthur@goldman-tribe.org>
2017-11-13 11:34:11 +0100

    Edited prime-factors to use keyword args and be more readable

--- prime-factors.lisp
+++ prime-factors.lisp
@@ -2,24 +2,38 @@
 
 (defun find-limit (n)
   (+ 1 (ceiling (sqrt n))))
+
 (defun factor? (n factor)
   (= (rem n factor) 0))
+
 (defun no-more-factors (n factors)
   (if (= n 1)
       factors
       (cons n factors)))
-(defun more-factors (n limit test-factor factors)
+
+(defun more-factors (n &key
+                         (limit (find-limit n))
+                         (test-factor 2)
+                         (factors '()))
   (if (factor? n test-factor)
       (let ((new-n (/ n test-factor)))
-        (prime-factors-iter
-         new-n
-         (find-limit new-n)
-         2
-         (cons test-factor factors)))
-      (prime-factors-iter n limit (+ test-factor 1) factors)))
-(defun prime-factors-iter (n limit test-factor factors)
-  (if (>= test-factor limit)
-      (no-more-factors n factors)
-      (more-factors n limit test-factor factors)))
+        (prime-factors-iter new-n
+                            :factors (cons test-factor factors)))
+      (prime-factors-iter n
+                          :limit limit
+                          :test-factor (+ test-factor 1)
+                          :factors factors)))
+
+(defun prime-factors-iter (n &key
+                               (limit (find-limit n))
+                               (test-factor 2)
+                               (factors '()))
+    (if (>= test-factor limit)
+        (no-more-factors n factors)
+        (more-factors n
+                      :limit limit
+                      :test-factor test-factor
+                      :factors factors)))
+
 (defun prime-factors (n)
-  (prime-factors-iter n (find-limit n) 2 '()))
+  (prime-factors-iter n))

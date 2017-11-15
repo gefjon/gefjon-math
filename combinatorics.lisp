841eb867f4f028cfa99aa12368b45bbd4860dd21
Arthur Goldman <arthur@goldman-tribe.org>
2017-11-15 14:53:03 +0100

    Now using a better factorial algorithm, thanks to Richard Fateman's paper
    https://people.eecs.berkeley.edu/~fateman/papers/factorial.pdf

--- combinatorics.lisp
+++ combinatorics.lisp
@@ -1,9 +1,16 @@
 (in-package :gmath)
 
-(defun factorial (n &optional (acc 1))
-  (declare (integer n acc))
-  (if (<= n 1) acc
-      (factorial (- n 1) (* acc n))))
+(defun factorial (n)
+  (let ((shift 0))
+    (labels
+        ((iter (n m)
+           (cond ((and (evenp n) (> m 1))
+                  (incf shift (ash n -1))
+                  (iter (ash n -1) (ash m -1)))
+                 ((<= n m) n)
+                 (t (* (iter n (ash m 1))
+                       (iter (- n m) (ash m 1)))))))
+      (ash (iter n 1) shift))))
 
 (defun permute (n k)
   (declare (integer n k))

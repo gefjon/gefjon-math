b5788bafd0e17d976edbad9831ee6ce4be6642dc
Arthur Goldman <arthur@goldman-tribe.org>
2017-11-22 14:24:02 +0100

    Docstrings and type stuff

--- combinatorics.lisp
+++ combinatorics.lisp
@@ -1,6 +1,8 @@
 (in-package :gmath)
 
 (defun factorial (n)
+  "Compute n! using the algorith described at https://people.eecs.berkeley.edu/~fateman/papers/factorial.pdf"
+  (check-type n integer)
   (let ((shift 0))
     (labels
         ((iter (n m)
@@ -12,14 +14,30 @@
                        (iter (- n m) (ash m 1)))))))
       (ash (iter n 1) shift))))
 
-(defun permute (n k)
+(defun permute-unchecked (n k)
+  "The algorithm behind gmath:permute"
   (declare (integer n k))
   (if (> k n) 0
-      (/ (factorial n)
-         (factorial (- n k)))))
+      (loop for i downfrom n to (+ (- n k) 1)
+         for x = n then (* x i)
+         finally (return x))))
+
+(defun combine-unchecked (n k)
+  "The algorithm behind gmath:combine"
+  (declare (integer n k))
+  (if (> k n) 0
+      (let ((k (min k (- n k))))
+            (/ (permute-unchecked n k)
+              (factorial k)))))
+
+(defun permute (n k)
+  "Compute nPk with a pretty bad O(n) algorithm"
+  (check-type n integer)
+  (check-type k integer)
+  (permute-unchecked n k))
 
 (defun combine (n k)
-  (declare (integer n k))
-  (if (> k n) 0
-      (/ (factorial n)
-         (* (factorial k) (factorial (- n k))))))
+  "Compute nCk with a pretty bad O(n) algorithm"
+  (check-type n integer)
+  (check-type k integer)
+  (combine-unchecked n k))
--- prime-factors.lisp
+++ prime-factors.lisp
@@ -1,6 +1,7 @@
 (in-package :gmath)
 
 (defun find-limit (n)
+  "Returns the largest possible prime factor of n other than n"
   (declare (integer n))
   ;; A composite number cannot have any prime factors larger than
   ;; its square root, so we only search up to the first integer
@@ -8,10 +9,12 @@
   (+ 1 (ceiling (sqrt n))))
 
 (defun factor-p (n factor)
+  "t if factor|n, nil otherwise"
   (declare (integer n factor))
   (= (rem n factor) 0))
 
 (defun no-more-factors (n factors)
+  "Check if n is prime and return the list of factors from gmath:prime-factors"
   (declare (integer n)
            (list factors))
   ;; If passed some n > 1, n is a prime factor of whatever number we
@@ -25,7 +28,7 @@
                                         limit
                                         factors
                                         factor-adjust)
-  ;; This macro is where all the hard work happens
+  "Check if test-factor is too large to be a factor of n, and otherwise if it is a factor. If it is not, try again with (+ test-factor factor-adjust)"
   (let ((limit-name (gensym)))                   ; limit is the only var that
                                                  ; gets passed as an evaluated
                                                  ; form which may have side
@@ -34,14 +37,15 @@
        (if (>= ,test-factor ,limit-name)          ; if we've passed the limit,
            (no-more-factors ,n ,factors)         ; return
            (if (factor-p ,n ,test-factor)
-               (first-factor (/ ,n ,test-factor) ; first-factor checks 2
+               (factor-start-iter (/ ,n ,test-factor) ; first-factor checks 2
                              (cons ,test-factor ,factors))
-               (more-factors ,n                  ; whereas more-factors proceeds
+               (factor-continue-iter ,n          ; whereas more-factors proceeds
                              ,limit-name         ; across the odd numbers
                              (+ ,test-factor ,factor-adjust)
                              ,factors))))))
 
-(defun first-factor (n factors)
+(defun factor-start-iter (n factors)
+  "Called during gmath:prime-factors to check if 2 is a factor"
   (declare (integer n)
            (list factors))
   ;; This function exists because 2 is the only even prime. We want to test
@@ -53,10 +57,11 @@
                                 factors
                                 1))
 
-(defun more-factors (n
+(defun factor-continue-iter (n
                      limit
                      test-factor
                      factors)
+  "Called during gmath:prime-factors to check odd factors > 2"
   (declare (integer n limit test-factor)
            (list factors))
   (check-limit-and-check-factor n
@@ -68,4 +73,4 @@
 (defun prime-factors (n)
   "Return a list of the prime factors of n"
   (check-type n integer)
-  (first-factor n '()))
+  (factor-start-iter n '()))

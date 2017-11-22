ef9a66f5e45f22f6867bc93b8236ef6545a6faa9
Arthur Goldman <arthur@goldman-tribe.org>
2017-11-22 13:20:35 +0100

    Docstring for prime-factors

--- prime-factors.lisp
+++ prime-factors.lisp
@@ -2,6 +2,9 @@
 
 (defun find-limit (n)
   (declare (integer n))
+  ;; A composite number cannot have any prime factors larger than
+  ;; its square root, so we only search up to the first integer
+  ;; greater than that square root
   (+ 1 (ceiling (sqrt n))))
 
 (defun factor-p (n factor)
@@ -11,6 +14,8 @@
 (defun no-more-factors (n factors)
   (declare (integer n)
            (list factors))
+  ;; If passed some n > 1, n is a prime factor of whatever number we
+  ;; were factoring in the first place
   (if (= n 1)
       factors
       (cons n factors)))
@@ -20,32 +25,38 @@
                                         limit
                                         factors
                                         factor-adjust)
-  (let ((limit-name (gensym)))
+  ;; This macro is where all the hard work happens
+  (let ((limit-name (gensym)))                   ; limit is the only var that
+                                                 ; gets passed as an evaluated
+                                                 ; form which may have side
+                                                 ; effects or be expensive
     `(let ((,limit-name ,limit))
-       (if (>= ,test-factor ,limit-name)
-           (no-more-factors ,n ,factors)
+       (if (>= ,test-factor ,limit-name)          ; if we've passed the limit,
+           (no-more-factors ,n ,factors)         ; return
            (if (factor-p ,n ,test-factor)
-               (first-factor (/ ,n ,test-factor)
-                             :factors (cons ,test-factor ,factors))
-               (more-factors ,n
-                             :limit ,limit-name
-                             :test-factor (+ ,test-factor ,factor-adjust)
-                             :factors ,factors))))))
+               (first-factor (/ ,n ,test-factor) ; first-factor checks 2
+                             (cons ,test-factor ,factors))
+               (more-factors ,n                  ; whereas more-factors proceeds
+                             ,limit-name         ; across the odd numbers
+                             (+ ,test-factor ,factor-adjust)
+                             ,factors))))))
 
-(defun first-factor (n &key
-                         (factors '()))
+(defun first-factor (n factors)
   (declare (integer n)
            (list factors))
+  ;; This function exists because 2 is the only even prime. We want to test
+  ;; 2 as a factor, but we also want to count only across the odd numbers
+  ;; because otherwise we're just doing twice the work for nothing
   (check-limit-and-check-factor n
                                 2
                                 (find-limit n)
                                 factors
                                 1))
 
-(defun more-factors (n &key
-                         (limit (find-limit n))
-                         (test-factor 3)
-                         (factors '()))
+(defun more-factors (n
+                     limit
+                     test-factor
+                     factors)
   (declare (integer n limit test-factor)
            (list factors))
   (check-limit-and-check-factor n
@@ -55,5 +66,6 @@
                                 2))
 
 (defun prime-factors (n)
-  (declare (integer n))
-  (first-factor n))
+  "Return a list of the prime factors of n"
+  (check-type n integer)
+  (first-factor n '()))
